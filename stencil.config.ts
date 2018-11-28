94ca5f6395b0755ac536c2e09171363a0b55101b
Stephane Moreau <st.moreau@outlook.com.gr>
2018-11-28 16:45:02 +0000

    background-image conf

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,6 +2,10 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'mycomponent',
+  copy: [{
+    src: "**/*.jpeg",
+    dest: "./images"
+  }],
   outputTargets:[
     { type: 'dist' },
     { type: 'docs' },
